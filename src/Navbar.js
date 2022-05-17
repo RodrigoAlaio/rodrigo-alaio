8a0b22cea5a056a6cd7fd5d6edfde002e08d3e31
rodrigo <rodrigoalaio0@gmail.com>
2022-05-16 22:50:43 -0300

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Navbar = () =>{
+    return(
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        <a class="nav-link" href="#">Arte Blanco</a>
+        <a class="nav-link" href="#">Arte Gris</a>
+        <a class="nav-link disabled">Arte Negro</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+        </div>
+    );
+}
+export default Navbar;
